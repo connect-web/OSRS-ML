8bf781879b9a333e20092d59bfcaaaafdabef137
Elliot <141949359+webElliot@users.noreply.github.com>
2024-05-16 09:17:15 +0100

    added to PCA Class: k-means clustering with the ability to plot clusters

--- entities/features/pca.py
+++ entities/features/pca.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from sklearn.preprocessing import StandardScaler
 import plotly.express as px
-
-class Pca:
+from sklearn.cluster import KMeans
+class BasePca:
     print_variance = False
 
     def __init__(self, df, skills, minigames = None, n_components=None):
@@ -75,7 +75,7 @@ class Pca:
         plt.ylabel('Second Principal Component')
         plt.show()
 
-    def plot_interactive(self, activity=None):
+    def plot_interactive(self, activity=None, x_range = None, y_range = None):
         """
         Plots an interactive PCA graph with pid's included in points to recognise patterns in the data.
 
@@ -106,6 +106,13 @@ class Pca:
 
         fig.update_traces(marker=dict(size=5), selector=dict(mode='markers'))
         fig.update_layout(template="plotly_dark")
+
+        # Update the x and y range if specified
+        if x_range is not None:
+            fig.update_xaxes(range=x_range)
+        if y_range is not None:
+            fig.update_yaxes(range=y_range)
+
         fig.show()
         return pca_df
 
@@ -140,3 +147,118 @@ class Pca:
         plt.title('Explained Variance by Different PCA Components')
         plt.grid(True)
         plt.show()
+
+    def kmeans_cluster(self, n_clusters):
+        """
+        Performs K-means clustering on the PCA results and adds the cluster labels to the DataFrame.
+
+        :param n_clusters: The number of clusters to form.
+        :return: DataFrame with cluster labels.
+        """
+        # Perform K-means clustering
+        kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+        self.df['Cluster'] = kmeans.fit_predict(self.X_r)
+
+        # Create a DataFrame for the PCA results
+        pca_df = pd.DataFrame(data=self.X_r, columns=[f'PC{i + 1}' for i in range(self.X_r.shape[1])])
+        pca_df['Banned'] = self.y
+        pca_df['Cluster'] = self.df['Cluster']
+        pca_df['pid'] = self.df['pid'] if 'pid' in self.df.columns else 'PID not available'
+
+        return pca_df
+
+    def plot_clusters(self, n_clusters):
+        """
+        Plots the PCA results with K-means clusters.
+
+        :param n_clusters: The number of clusters to form.
+        :return: None
+        """
+        pca_df = self.kmeans_cluster(n_clusters)
+
+        fig = px.scatter(pca_df, x='PC1', y='PC2', color='Cluster',
+                         hover_data=['pid', 'Banned'],
+                         title=f'PCA with {n_clusters} K-means Clusters')
+        fig.update_traces(marker=dict(size=5), selector=dict(mode='markers'))
+        fig.update_layout(template="plotly_dark")
+        fig.show()
+
+class Pca(BasePca):
+    def __init__(self, df, skills, minigames=None, n_components=None):
+        super().__init__(df, skills, minigames, n_components)
+
+class PcaAgg(BasePca):
+    def __init__(self, df, skills, skills_agg, minigames=None, minigames_agg=None, extra_features=None,
+                 n_components=None):
+        """
+        Initializes the Pca Class
+
+        :param df: The Pandas Dataframe
+        :param skills: Default features
+        :param minigames: Optional features
+        :param n_components: The number of components to create the features with. Default is None for Elbow plot
+        """
+        super().__init__(df, skills, minigames, n_components)
+
+        self.df, self.n_components = df, n_components
+        self.skills, self.minigames = skills, minigames
+        self.skills_agg, self.minigames_agg = skills_agg, minigames_agg
+        self.extra_features = extra_features
+
+
+        self.y = self.df['Banned']
+
+        self.scaler_skills = StandardScaler()
+        self.scaler_minigames = StandardScaler()
+
+        self.scaler_skills_agg = StandardScaler()
+        self.scaler_minigames_agg = StandardScaler()
+
+        self.scaler_extra_features = StandardScaler()
+
+
+
+    def run(self):
+        """
+        Scales the data for skills & minigames separately
+        Runs PCA on the data
+        :return:
+        """
+        scaled_skills = self.scaler_skills.fit_transform(self.df[self.skills])
+        scaled_skills_agg = self.scaler_skills_agg.fit_transform(self.df[self.skills_agg])
+
+
+        # Convert scaled arrays back to DataFrames
+        scaled_skills_df = pd.DataFrame(scaled_skills, columns=self.skills)
+        scaled_skills_agg_df = pd.DataFrame(scaled_skills_agg, columns=self.skills_agg)
+
+        conc_df = pd.concat([scaled_skills_df, scaled_skills_agg_df], axis=1)
+
+        # Concatenate scaled dataframes
+        if self.minigames is not None:
+            scaled_minigames = self.scaler_minigames.fit_transform(self.df[self.minigames])
+            scaled_minigames_df = pd.DataFrame(scaled_minigames, columns=self.minigames)
+            conc_df = pd.concat([conc_df, scaled_minigames_df], axis=1)
+
+        if self.minigames_agg is not None:
+            scaled_minigames_agg = self.scaler_minigames_agg.fit_transform(self.df[self.minigames_agg])
+            scaled_minigames_agg_df = pd.DataFrame(scaled_minigames_agg, columns=self.minigames_agg)
+            conc_df = pd.concat([conc_df, scaled_minigames_agg_df], axis=1)
+
+        if self.extra_features is not None:
+            scaled_extra_features = self.scaler_extra_features.fit_transform(self.df[self.extra_features])
+            scaled_extra_features_df = pd.DataFrame(scaled_extra_features, columns=self.extra_features)
+            conc_df = pd.concat([conc_df, scaled_extra_features_df], axis=1)
+
+
+
+        self.X_scaled = conc_df
+
+        # Setup PCA
+        self.pca = PCA(n_components=self.n_components)
+        self.X_r = self.pca.fit_transform(self.X_scaled)
+
+        # Explained variance
+        if self.print_variance:
+            print('explained variance ratio (first two components):', self.pca.explained_variance_ratio_[:2])
+            print('sum of explained variance (first two components): ', sum(self.pca.explained_variance_ratio_[:2]))
