393c84e9cc788006740e4824cbfe9c261c0097ff
Elliot <141949359+webElliot@users.noreply.github.com>
2024-05-16 10:05:37 +0100

    Pca simplified by removing PcaAgg & adding a scaling function to scale the X axis based on kwargs groups accordingly

--- entities/analysis/pca.py
+++ entities/analysis/pca.py
@@ -3,51 +3,37 @@ from sklearn.decomposition import PCA
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-from sklearn.preprocessing import StandardScaler
 import plotly.express as px
 from sklearn.cluster import KMeans
+from entities.preprocessing.scaling import stats_scaling
+
 class Pca:
     print_variance = False
 
-    def __init__(self, df, skills, minigames = None, n_components=None):
+    def __init__(self, df, n_components=None, **kwargs):
         """
         Initializes the Pca Class
 
         :param df: The Pandas Dataframe
-        :param skills: Default features
-        :param minigames: Optional features
         :param n_components: The number of components to create the features with. Default is None for Elbow plot
+        :param kwargs: The feature group categories that are columns in the dataframe to be scaled as a group.
         """
+
         self.df, self.n_components = df, n_components
-        self.skills, self.minigames = skills, minigames
+
+        feature_groups = [kwargs[key] for key in kwargs if kwargs[key]]  # Filter out None or empty lists
+        self.X_scaled = stats_scaling(df, feature_groups=feature_groups)
 
         self.y = self.df['Banned']
 
-        self.scaler_skills = StandardScaler()
-        self.scaler_minigames = StandardScaler()
+        self.pca = PCA(n_components=self.n_components)
 
     def run(self):
         """
-        Scales the data for Skills & Minigames separately
-        Runs PCA on the data
+        Fits the scaled data to the pca
         :return:
         """
-        scaled_skills = self.scaler_skills.fit_transform(self.df[self.skills])
 
-        # Convert scaled arrays back to DataFrames
-        scaled_skills_df = pd.DataFrame(scaled_skills, columns=self.skills)
-
-
-        # Concatenate scaled dataframes
-        if self.minigames is not None:
-            scaled_minigames = self.scaler_minigames.fit_transform(self.df[self.minigames])
-            scaled_minigames_df = pd.DataFrame(scaled_minigames, columns=self.minigames)
-            self.X_scaled = pd.concat([scaled_skills_df, scaled_minigames_df], axis=1)
-        else:
-            self.X_scaled = scaled_skills_df
-
-        # Setup PCA
-        self.pca = PCA(n_components=self.n_components)
         self.X_r = self.pca.fit_transform(self.X_scaled)
 
         # Explained variance
@@ -116,21 +102,6 @@ class Pca:
         fig.show()
         return pca_df
 
-    def plot_interactive_3d(self):
-        """
-        Plots an interactive 3d scatter plot of the top 3 PCA components
-
-        :return: None
-        """
-        pca_df = pd.DataFrame(data=self.X_r, columns=[f'PC{i + 1}' for i in range(self.X_r.shape[1])])
-        pca_df['Banned'] = self.y
-        pca_df['pid'] = self.df['pid'] if 'pid' in self.df.columns else 'PID not available'
-
-        fig = px.scatter_3d(pca_df, x='PC1', y='PC2', z='PC3', color='Banned',
-                            hover_data=['pid', 'Overall'])
-        fig.update_layout(template="plotly_dark")
-        fig.show()
-
     def elbow_plot(self):
         """
         Plots an elbow graph to visualize the optimal number of components to capture important features.
@@ -182,79 +153,3 @@ class Pca:
         fig.update_traces(marker=dict(size=5), selector=dict(mode='markers'))
         fig.update_layout(template="plotly_dark")
         fig.show()
-
-class PcaAgg(Pca):
-    def __init__(self, df, skills, skills_agg, minigames=None, minigames_agg=None, extra_features=None,
-                 n_components=None):
-        """
-        Initializes the Pca Class
-
-        :param df: The Pandas Dataframe
-        :param skills: Default features
-        :param minigames: Optional features
-        :param n_components: The number of components to create the features with. Default is None for Elbow plot
-        """
-        super().__init__(df, skills, minigames, n_components)
-
-        self.df, self.n_components = df, n_components
-        self.skills, self.minigames = skills, minigames
-        self.skills_agg, self.minigames_agg = skills_agg, minigames_agg
-        self.extra_features = extra_features
-
-
-        self.y = self.df['Banned']
-
-        self.scaler_skills = StandardScaler()
-        self.scaler_minigames = StandardScaler()
-
-        self.scaler_skills_agg = StandardScaler()
-        self.scaler_minigames_agg = StandardScaler()
-
-        self.scaler_extra_features = StandardScaler()
-
-
-
-    def run(self):
-        """
-        Scales the data for skills & minigames separately
-        Runs PCA on the data
-        :return:
-        """
-        scaled_skills = self.scaler_skills.fit_transform(self.df[self.skills])
-        scaled_skills_agg = self.scaler_skills_agg.fit_transform(self.df[self.skills_agg])
-
-
-        # Convert scaled arrays back to DataFrames
-        scaled_skills_df = pd.DataFrame(scaled_skills, columns=self.skills)
-        scaled_skills_agg_df = pd.DataFrame(scaled_skills_agg, columns=self.skills_agg)
-
-        conc_df = pd.concat([scaled_skills_df, scaled_skills_agg_df], axis=1)
-
-        # Concatenate scaled dataframes
-        if self.minigames is not None:
-            scaled_minigames = self.scaler_minigames.fit_transform(self.df[self.minigames])
-            scaled_minigames_df = pd.DataFrame(scaled_minigames, columns=self.minigames)
-            conc_df = pd.concat([conc_df, scaled_minigames_df], axis=1)
-
-        if self.minigames_agg is not None:
-            scaled_minigames_agg = self.scaler_minigames_agg.fit_transform(self.df[self.minigames_agg])
-            scaled_minigames_agg_df = pd.DataFrame(scaled_minigames_agg, columns=self.minigames_agg)
-            conc_df = pd.concat([conc_df, scaled_minigames_agg_df], axis=1)
-
-        if self.extra_features is not None:
-            scaled_extra_features = self.scaler_extra_features.fit_transform(self.df[self.extra_features])
-            scaled_extra_features_df = pd.DataFrame(scaled_extra_features, columns=self.extra_features)
-            conc_df = pd.concat([conc_df, scaled_extra_features_df], axis=1)
-
-
-
-        self.X_scaled = conc_df
-
-        # Setup PCA
-        self.pca = PCA(n_components=self.n_components)
-        self.X_r = self.pca.fit_transform(self.X_scaled)
-
-        # Explained variance
-        if self.print_variance:
-            print('explained variance ratio (first two components):', self.pca.explained_variance_ratio_[:2])
-            print('sum of explained variance (first two components): ', sum(self.pca.explained_variance_ratio_[:2]))
