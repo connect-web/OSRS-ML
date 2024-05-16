fb4679bc0390b759876d29d8690b881f037c9208
Elliot <141949359+webElliot@users.noreply.github.com>
2024-05-16 15:08:54 +0100

    added models: gbm_model, lgbm_model, xgb_model, logistic_regression_model, svm_model

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,15 @@
 from .random_forest import (
     rf_model, et_model
 )
+
+from .gradient_boosting_machines import (
+    gbm_model, lgbm_model, xgb_model
+)
+
+from .logistic_regression import (
+    logistic_regression_model
+)
+
+from .support_vector_machines import (
+    svm_model
+)
