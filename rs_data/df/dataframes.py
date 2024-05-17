915b3a34c5e400f9c58811fd65b3e9736798a976
Elliot <141949359+webElliot@users.noreply.github.com>
2024-05-17 14:12:04 +0100

    direct dynamic dataframe function with options for skill processing

---
+++ rs_data/df/dataframes.py
@@ -0,0 +1,26 @@
+from rs_data.database import (
+    get_skill_hiscore
+)
+
+from .enums import SkillType
+
+
+def get_dataframe(activity: str, limit: int, offset: int = 0, aggregate: bool = True,
+                  skill_type: SkillType = SkillType.EXPERIENCE):
+    """
+    Fetches users from the Hiscore rankings for Q1 2024 in the order of activity experience DESC.
+    Rows are then converted into a dataframe and the formatter object is returned with the column names
+    separated into group categories for scaling.
+
+    :param activity: The skill activity.
+    :param limit: The maximum amount of users.
+    :param offset: The offset from the start of the users.
+    :param aggregate: Optional boolean to return the Experience gained for Q1 2024. False will only include user experience at end of Q1 2024.
+    :param skill_type: The type of processing to apply on skills, either ['EXPERIENCE', 'LEVELS', 'RATIO']
+    :return:
+    """
+    df, formatter = get_skill_hiscore(activity, limit=limit, offset=offset, aggregate=aggregate)
+
+    # apply data processing
+    df = skill_type.apply(df, aggregate)
+    return df, formatter
