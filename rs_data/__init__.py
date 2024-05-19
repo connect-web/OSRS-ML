c3e305d02c213e877035b6b2239e9a22ea341b9f
Elliot <141949359+webElliot@users.noreply.github.com>
2024-05-19 13:22:11 +0100

    some analysis

--- rs_data/__init__.py
+++ rs_data/__init__.py
@@ -1,7 +1,9 @@
 from .df import (
     get_dataframe, SkillType,
-    export_dataframe, export_usernames
+    export_dataframe, export_usernames,
 )
+
+from .database import get_players
 
 from .pipelines import (
     evaluate_pipelines, pipeline_configs
