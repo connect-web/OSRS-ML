d2071aabde322f43c9acea7a4eb961c665dba2fe
Elliot <141949359+webElliot@users.noreply.github.com>
2024-05-27 15:24:41 +0100

    added EXTRA FEATURES , rename aggregate, rename_player_live to the processing module.

--- rs_data/database/rs_processing/__init__.py
+++ rs_data/database/rs_processing/__init__.py
@@ -1,6 +1,9 @@
-from .leaderboards import Leaderboards
+from .leaderboards import Leaderboards, rename_aggregate, rename_player_live
+
+
+
 from .level_calculator import calculator
-from .row_format import RowFormat, RowFormatAdvanced
+from .row_format import RowFormat, RowFormatAdvanced, EXTRA_FEATURES
 from .skill_converter import (
     df_skill_ratio_basic, df_skill_ratio_advanced,
     df_levels_basic, df_levels_advanced
