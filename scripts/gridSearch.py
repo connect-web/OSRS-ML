6ee1cde8a5b472f1d07852f6b2d2143c6bf1f74c
Elliot <141949359+webElliot@users.noreply.github.com>
2024-06-01 19:19:37 +0100

    added new ban feature tracker

--- scripts/gridSearch.py
+++ scripts/gridSearch.py
@@ -2,6 +2,8 @@ from ml import (
     GridSearchActivity,
     get_best_roc_auc_experiments
 )
+from rs_data.database.rs_processing import Leaderboards
+
 
 import pandas as pd
 
@@ -12,21 +14,38 @@ pd.set_option('display.max_columns', None)  # Display all columns
 
 def main():
     experiments = get_best_roc_auc_experiments()
-    print(experiments)
+    total_activities = Leaderboards.get_skill_names(keep_overall=True) + Leaderboards.get_minigame_names()
 
+    for activity_name in total_activities:
+        try:
+            run_grid_search(experiments, activity_name)
+        except Exception as e:
+            print(f'{activity_name} : {e}')
 
-def run_dks_experiment():
-    activity_name = 'Dagannoth Prime'
-
-    experiments = get_best_roc_auc_experiments()
+def run_grid_search(experiments, activity_name, new_ban_data = False):
     df = experiments[experiments['activity'] == activity_name]
 
     grid_searcher = GridSearchActivity(
         activity=activity_name,
         classifier_name=df['params.Classifier'].iloc[0],
-        n_components=df['params.PCA_n_components'].iloc[0]
+        n_components=df['params.PCA_n_components'].iloc[0],
+        new_ban_data=new_ban_data
     )
     grid_searcher.run()
 
+def run_dks_experiment():
+    activity_name = 'Dagannoth Prime'
+    experiments = get_best_roc_auc_experiments()
+
+    run_grid_search(experiments, activity_name)
+
+def run_dks_experiment_new_dataset():
+    activity_name = 'Dagannoth Prime'
+    experiments = get_best_roc_auc_experiments()
+
+    run_grid_search(experiments, activity_name)
+
+
+
 if __name__ == "__main__":
-    run_dks_experiment()
+    run_dks_experiment_new_dataset()
